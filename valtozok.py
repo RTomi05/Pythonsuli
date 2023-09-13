e260a6cd48a986bc4043ce68bcf9b78372d9bfda
Rengel Tamás <rengelt22@ADY-NAGYATAD.SULINET.HU>
2023-09-13 08:09:51 +0200

    new

---
+++ valtozok.py
@@ -0,0 +1,32 @@
+#változók próbálgatása
+
+
+szam=42
+print(szam)
+
+valosSzam=2.3
+print (valosSzam)
+
+szoveg="hahahahaaa"
+print (szoveg)
+
+sutANap=True
+print(sutANap)
+
+szam=szam+2
+print(szam)
+
+szam=szam+2
+print(szam)
+
+print(szam-10)
+print(szam*10)
+print(szam/10)
+
+
+szam=12
+print(szam%9)
+
+print(szam//10)
+print(szam//13)
+print(szam//5)
