6f32b9d15e6a40444ea87e45a9bf54f0944b59c6
Rengel Tamás <rengelt22@ADY-NAGYATAD.SULINET.HU>
2023-10-09 14:05:08 +0200

    new

---
+++ valami.py
@@ -0,0 +1,37 @@
+lista=["a", "b", "z", "h", "c", "i", "o"]
+
+print(lista)
+
+print("#"*50)
+
+for elem in lista:
+    print(elem)
+
+print("#"*50)
+    
+for i in range(len(lista)):
+    print(lista[i])
+
+print("#"*50)
+
+for elem in lista[:3]:
+    print(elem)
+
+print("#"*50)
+
+for elem in lista[3:]:
+    print(elem)
+
+print("#"*50)
+
+for elem in lista[-2:]:
+    print(elem)
+
+print("#"*50)
+
+for elem in lista[::-1]:
+    print(elem)
+
+
+lista2=lista[1:-1]
+print(lista2)
