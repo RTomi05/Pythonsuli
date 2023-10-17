03e343f2d58fb8c483202d7085a65920b00f3f7b
Rengel Tamás <rengelt22@ADY-NAGYATAD.SULINET.HU>
2023-10-17 12:35:25 +0200

    string

---
+++ stringek/stringek.py
@@ -0,0 +1,22 @@
+
+for x in "banana":
+    print(x)
+
+
+txt="Ez az elem bent van-e"
+print("elem" in txt)
+
+a="Hello World!"
+print(a.upper())
+print(a.lower())
+print(a.strip())
+print(a.replace("H", "J"))
+
+#trükk
+print(a.replace("l", ""))
+
+#karaktereket számol
+print(len(a))
+
+b=a.split("l")
+print(b)
