368b3854a294f32c73b8e3d62270a1afe3abd8fa
RTomi05 <rengel.tamas.22@ady-nagyatad.hu>
2023-12-12 16:00:10 +0100

    plusz

---
+++ plusz/szorka.py
@@ -0,0 +1,26 @@
+
+print("Rengel Tamás")
+
+szam = 35
+szam2 = 47
+
+print(szam*2 + szam2*2)
+
+#VAGY
+
+szam = 35
+szam2 = 47
+szamK = 2
+
+szamV = szam*szamK
+szamV2 = szam2*szamK
+
+szamOsszeg = szamV + szamV2
+print(szamOsszeg)
+
+#VAGY
+
+szamok=[35,47,2]
+
+szamOsszeg = szamok[0]*szamok[2] + szamok[1]*szamok[2]
+print(szamOsszeg)
