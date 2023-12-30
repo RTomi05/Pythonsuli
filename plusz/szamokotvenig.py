f88f15d31c371aed4ff6723c7000c8c7623a7887
RTomi05 <rengel.tamas.22@ady-nagyatad.hu>
2023-12-30 15:42:03 +0100

    plusz

---
+++ plusz/szamokotvenig.py
@@ -0,0 +1,23 @@
+
+#Írassuk ki a számokat 1-től – 50-ig. 
+
+szamok=[]
+i = 0
+
+for i in range(50):
+    i += 1
+    szamok.append(i)
+
+print(szamok)
+
+#VAGY
+
+szamok1=[]
+
+i = 0
+for i in range(50):
+    i = i+1
+    szamok1.append(i)
+
+print(szamok1)
+
