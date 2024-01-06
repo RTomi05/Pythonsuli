2e3354c326f30a42ea7e60d087e6d74372a11a76
RTomi05 <rengel.tamas.22@ady-nagyatad.hu>
2024-01-06 14:00:26 +0100

    javitas

--- plusz/huvcem.py
+++ plusz/huvcem.py
@@ -1,7 +1,7 @@
 
 #A nemzetközi hüvelyk: 1″ = 2,54 cm Kérjen be egy hüvelyk értéket, majd írja ki centiméterben. 
 
-mennyiseg = float(input("A bekért adat centiméterben: "))
+mennyiseg = float(input("A bekért adat hüvelykben: "))
 
 txt = "A megadott mennyiség centiméterben kifejezve: {ertek}"
 print(txt.format(ertek = mennyiseg * 2.54))
--- plusz/oszti.py
+++ plusz/oszti.py
@@ -3,9 +3,10 @@ print("Rengel Tamás")
 
 a = 3.7
 b = 4.8
-c = 2.4
+c = 2.5
 
-abc = a + b +c
-abcOszt = abc / 2
-
-print("A számok összege:", abc,"\n""Az összeg fele:", abcOszt)
+txt = "A számok összege: {abc}"
+print(txt.format(abc = a + b + c))
+abc = a + b + c
+txt2 = "Az összeg fele: {abcOszt}"
+print(txt2.format(abcOszt = abc / 2))
