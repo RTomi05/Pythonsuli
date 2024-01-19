8584071510f5538837211c6af99b096173de43b0
RTomi05 <rengel.tamas.22@ady-nagyatad.hu>
2024-01-19 08:37:58 +0100

    plusz

--- plusz/tetrafel.py
+++ plusz/tetrafel.py
@@ -1,10 +1,12 @@
 
 #Adott egy tetraéder. Számoltassa ki a tetraéder felszínét.
 
-a = input("Adja meg a tetraédert határoló háromszögek területét: ")
-b = input("Adja meg a tetraédert határoló háromszögek területét: ")
-c = input("Adja meg a tetraédert határoló háromszögek területét: ")
-d = input("Adja meg a tetraédert határoló háromszögek területét: ")
+abcd1 = 0
+
+a = int(input("Adja meg a tetraédert határoló háromszögek területét: "))
+b = int(input("Adja meg a tetraédert határoló háromszögek területét: "))
+c = int(input("Adja meg a tetraédert határoló háromszögek területét: "))
+d = int(input("Adja meg a tetraédert határoló háromszögek területét: "))
 
 txt = "A tetraéder felszíne: {abcd1}"
 print(txt.format(abcd1 = a + b + c + d))
