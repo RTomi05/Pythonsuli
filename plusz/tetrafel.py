32c9aa665bae5d3532cddc9f3009b816d8d84438
RTomi05 <rengel.tamas.22@ady-nagyatad.hu>
2024-01-19 08:45:53 +0100

    plusz

--- plusz/tetrafel.py
+++ plusz/tetrafel.py
@@ -10,3 +10,15 @@ d = int(input("Adja meg a tetraédert határoló háromszögek területét: "))
 
 txt = "A tetraéder felszíne: {abcd1}"
 print(txt.format(abcd1 = a + b + c + d))
+
+
+#VAGY
+
+lista=[]
+
+for x in range(4):
+    t = int(input("Ajda meg a tetraédert határoló háromszögek területét: "))
+    lista.append(t)
+
+text = "A tetraéder felszíne: {t1}"
+print(text.format(t1 = sum(lista)))
