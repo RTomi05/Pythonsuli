e58434a2c56770cda7a0d788258b217f2bae0acd
RTomi05 <rengel.tamas.22@ady-nagyatad.hu>
2024-01-20 19:33:17 +0100

    refresh

--- szovegeng.py
+++
@@ -1,9 +0,0 @@
-text={"Reggel felébredtem. Mit tegyek?":"Reggel felébredtem. Mit tegyek most?",
-    "fogmosás":"fogmosás",
-    "reggeli":"reggeli",
-    "öltözés":"öltözés",
-    "66-os parancs":"66-os parancs",
-    "Elmegyek fogat mosni. Sikálom rendesen, ahogy kell!":"Elmegyek fogat mosni. Sikálom rendesen, ahogy kell!",
-    "Kellene valamit enni. Anya csinált valamit? Nézzük meg!":"Kellene valamit enni. Anya csinált valamit? Nézzük meg!",
-    "Kissé hűvös van. Kellene valami ruha. \nFelveszek egy nadrágot, meg egy pólót.":"Kissé hűvös van. Kellene valami ruha. \nFelveszek egy nadrágot, meg egy pólót.",
-     }
