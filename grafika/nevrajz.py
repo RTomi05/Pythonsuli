6415b5a2884fabb0f9dbb80e45d9973a7bd621c7
Rengel Tamás <rengelt22@ADY-NAGYATAD.SULINET.HU>
2024-01-22 14:06:30 +0100

    hfd

--- grafika/nevrajz.py
+++ grafika/nevrajz.py
@@ -23,21 +23,52 @@ TT=[0,0,
     100,0,
     0,0]
 
-AA=[0,0,
+TT2=[0,0,
     0,20,
-    30,20,
+    35,20,
     40,90,
     60,90,
-    70,20,
+    65,20,
     100,20,
     100,0,
     0,0]
 
+AA=[0,0,
+    0,20,
+    35,20,
+    40,90,
+    60,90,
+    65,20,
+    100,20,
+    100,0,
+    0,0]
+
+
+TT3 = []
+for e in TT2:
+	e = transzformaciok.nagyit(e,10)
+	e = transzformaciok.eltol(e,100,100)
+	#e = transzformaciok.forgat(e,45)
+    
+	TT3.append(e)
+
+
+for e in TT3:
+	canvas.create_line(e,width=5,fill="red")
+
+
+
 nevT = transzformaciok.eltol(TT,20,50)
+canvas.create_line(nevT,width=5,fill="red")
+nevT = transzformaciok.eltol(TT2,20,50)
 canvas.create_line(nevT,width=5,fill="red")
 
 nevA = transzformaciok.eltol(AA,150,50)
 canvas.create_line(nevA,width=5,fill="red")
+
+
+
+
 
 
 win.mainloop()
