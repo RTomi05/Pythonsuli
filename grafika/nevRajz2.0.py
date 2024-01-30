cc351f6c40c05f05fde7e706907b42c22a41180b
RTomi05 <rengel.tamas.22@ady-nagyatad.hu>
2024-01-30 12:35:33 +0100

    teljeskeresztnev

--- grafika/nevRajz2.0.py
+++ grafika/nevRajz2.0.py
@@ -13,6 +13,88 @@ canvas = Canvas(win, width=600, height=600)
 canvas.configure(bg="lightgray")
 canvas.pack(fill=BOTH, expand=1) #teljes ablakot kitölti
 
+
+
+tamas = [[0,0,
+    0,20,
+    35,20,
+    40,90,
+    60,90,
+    65,20,
+    100,20,
+    100,0,
+    0,0],
+    [0,0,
+    0,20,
+    35,20,
+    40,90,
+    60,90,
+    65,20,
+    100,20,
+    100,0,
+    0,0],
+    [302,280,
+    350,100,
+    400,280,
+    403,280
+    ],
+    [300,280,
+    315,280,
+    350,120,
+    380,280,
+    403,280
+    ],
+    [320,250,
+    375,250,
+    ],
+    [320,250,
+    375,250,
+    ],
+    [330,220,
+    370,220,
+    ],
+    [330,220,
+    370,220,
+    ],
+    [440,282,
+    440,97,
+    510,160,
+    580,97,
+    580,282
+    ],
+    [438,282,
+    450,282,
+    450,120,
+    510,180,
+    570,120,
+    570,282,
+    583,282],
+    [650,50,
+    660,90,
+    680,90,
+    670,50,
+    650,50],
+    [810,90,
+    750,90,
+    750,180,
+    810,180,
+    810,270,
+    750,270,
+    750,280,
+    825,280,
+    825,165,
+    760,165,
+    760,105,
+    825,105,
+    825,90,
+    800,90]]
+
+
+
+
+
+
+
 TT=[[0,0,
     0,20,
     35,20,
@@ -105,46 +187,17 @@ SS = [810,90,
 
 
 #eredeti megtartása (copy)
-A2szele = transzformaciok.masol(ASzele)
-A2szele = transzformaciok.eltol(A2szele,320,0)
-A2kozepe = transzformaciok.masol(AKozepe)
-A2kozepe = transzformaciok.eltol(A2kozepe,320,0)
-A2Vonalka1 = transzformaciok.eltol(A2Vonalka1,320,0)
-A2Vonalka2 = transzformaciok.eltol(A2Vonalka2,320,0)
-
-
-
-#tamas = TT + ASzele + AKozepe + AVonalka1 + A2Vonalka1 + AVonalka2 + A2Vonalka2 + Mkulso + Mbelso + ekezet + SS
-#print(tamas)
 
 
 
 
-TT2=[]
-for e in TT:
-    e = transzformaciok.eltol(e,40,50)
-    e = transzformaciok.nagyit(e,2)
-    TT2.append(e)
-	
-for e in TT2:
-   canvas.create_line(e,width=5,fill="red")
 
-for e in TT:
-    e = transzformaciok.forgat(TT,0.1)
-    canvas.create_line(e,width=5,fill="green")
 
-canvas.create_line(ASzele,width=5,fill="red")
-canvas.create_line(AKozepe,width=5,fill="red")
-canvas.create_line(AVonalka1,width=5,fill="red")
-canvas.create_line(AVonalka2,width=5,fill="red")
-canvas.create_line(Mkulso,width=5,fill="red")
-canvas.create_line(Mbelso,width=5,fill="red")
-canvas.create_line(A2szele,width=5,fill="red")
-canvas.create_line(A2kozepe,width=5,fill="red")
-canvas.create_line(A2Vonalka1,width=5,fill="red")
-canvas.create_line(A2Vonalka2,width=5,fill="red")
-canvas.create_line(ekezet,width=5,fill="blue")
-canvas.create_line(SS,width=5,fill="red")
+for e in tamas:
+    e = canvas.create_line(e,width=5,fill="purple")
+
+
+
 
 
 
