206480ebfb1edddf52ea4256b587833989881d58
RTomi05 <rengel.tamas.22@ady-nagyatad.hu>
2024-02-02 08:46:27 +0100

    aha

--- grafika/Pong/pong.py
+++ grafika/Pong/pong.py
@@ -1,5 +1,20 @@
-#egyszerű pong játék
+#Egyszerű Pong Játék
 #Start: 2024. 01. 31.
 #End: 
 #Rengel Tamás
 
+
+from tkinter import *
+
+
+jatekHatter = "lightgray"
+
+win=Tk()
+win.geometry("600x600+100+20")
+win.title("Pong játék")
+canvas=Canvas(win, bg=jatekHatter)
+canvas.pack(fill = BOTH, expand = 1)
+
+
+canvas.create_oval(0,0,20,20, fill = "red")
+win.mainloop()
--- plusz/kod.py
+++ plusz/kod.py
@@ -4,8 +4,28 @@ print("Rengel Tamás")
 print("2024. 01. 22.")
 
 szam = int(input("Adja meg a kódszámot: "))
-while szam == szam:
-    if szam == 2248 or 1834 or 3823 :
-        print("Bejutott a rendszerbe!")
-    elif szam != 2248 or 1834 or 3823:
-        print("Sikertelen azonosítás!")
+engedelyek = 2248
+engedelyek2 = 1834
+engedelyek3 = 3823
+
+if szam == engedelyek:
+    print("Bejutott a rendszerbe!")
+elif szam == engedelyek2:
+    print("Bejutott a rendszerbe!")
+elif szam == engedelyek3:
+    print("Bejutott a rendszerbe!")
+else:
+    print("Sikertelen azonosítás!")
+
+
+#VAGY
+    
+
+szamok = [2248,1834,3823]
+
+szam2 = int(input("Adja meg a kódszámot: "))
+if szam2 in szamok:
+    print("Bejutott a rendszerbe!")
+
+else:
+    print("Sikertelen azonosítás!")
