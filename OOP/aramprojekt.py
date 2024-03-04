e7bf04c511ede543f04ed691c1350356d969bfac
Rengel Tamás <rengelt22@ady-nagyatad.sulinet.hu>
2024-03-04 13:37:49 +0100

    veg

--- OOP/aramprojekt.py
+++ OOP/aramprojekt.py
@@ -4,14 +4,14 @@ class Jel:
 	y = 0
 	meret = 100
 	szin = "black"
+
 	def __init__(self, x, y, meret, canvas):
 		self.x = x
 		self.y = y
 		self.meret = meret
-		self. r = self.meret * 0.06
-		#bekötési pont by Taki
-		self.bkp = [[self.x, self.y + self.meret * 0.5], [self.x + self.meret, self.y + self.meret * 0.5]]
-
+		self.r = self.meret * 0.06
+		#Bekötési pont by Taki
+		self.bkp = [[self.x, self.y+self.meret*0.5],[self.x+self.meret, self.y+self.meret*0.5],]
 		self.canvas = canvas
 		self.rajz()
 
@@ -20,25 +20,59 @@ class Jel:
 		self.canvas.create_rectangle(self.x, self.y, self.x + self.meret, self.y + self.meret, fill = "gray")
 
 		for egyVonal in vonalak:
-			self.canvas.create_line(egyVonal, width = self.meret * 0.03, fill = self.szin)
+			self.canvas.create_line(egyVonal, width = self.meret * 0.03, fill = self.szin)	
 			
 		for egyKor in korok:
-			self.canvas.create_oval(egyKor, outline = self.szin, width = self.meret * 0.03)
+			self.canvas.create_oval(egyKor, outline = self.szin, width= self.meret*0.03)
 
+	def vezetek(self,masik, sajatBKP=1, masikBKP=0):
 
-	def vezetek(self, masik, sajatBKP = 1, masikBKP = 0):
-		vonalak = [
+		if sajatBKP ==1 and masikBKP==0 and self.bkp[sajatBKP][0] < masik.bkp[masikBKP][0]:
+			vonalak = [
+				[
+					self.bkp[sajatBKP][0],self.bkp[sajatBKP][1],
+					(self.bkp[sajatBKP][0] + masik.bkp[masikBKP][0]) /2, self.bkp[sajatBKP][1],
+					(self.bkp[sajatBKP][0] + masik.bkp[masikBKP][0]) /2, masik.bkp[masikBKP][1],
+					masik.bkp[masikBKP][0],masik.bkp[masikBKP][1]
+				],
+			]
+
+		elif (sajatBKP ==1 and masikBKP==1) and self.bkp[sajatBKP][0] < masik.bkp[masikBKP][0]:
+			vonalak = [
 			[
-				self.bkp[sajatBKP][0], self.bkp[sajatBKP][1],
-				masik.bkp[masikBKP][0], masik.bkp[masikBKP][1],
+				self.bkp[sajatBKP][0],self.bkp[sajatBKP][1],
+				(self.bkp[sajatBKP][0] + masik.x) /2, self.bkp[sajatBKP][1],
+				(self.bkp[sajatBKP][0] + masik.x) /2, masik.y+masik.meret*1.2,
+				masik.x + masik.meret*1.2							,masik.y+masik.meret*1.2,
+				masik.x + masik.meret*1.2							,masik.bkp[masikBKP][1],
+				masik.bkp[masikBKP][0],masik.bkp[masikBKP][1],
 			],
 		]
-		
+		elif (sajatBKP ==1 and masikBKP==1) and self.x < masik.x < self.x + self.meret:
+			vonalak = [
+			[
+				self.bkp[sajatBKP][0],self.bkp[sajatBKP][1],
+				(self.bkp[sajatBKP][0] + masik.x) /2, self.bkp[sajatBKP][1],
+				(self.bkp[sajatBKP][0] + masik.x) /2, masik.y+masik.meret*1.2,
+				masik.x + masik.meret*1.2							,masik.y+masik.meret*1.2,
+				masik.x + masik.meret*1.2							,masik.bkp[masikBKP][1],
+				masik.bkp[masikBKP][0],masik.bkp[masikBKP][1],
+			],
+		]
+		else:
+			vonalak = [
+				[
+					self.bkp[sajatBKP][0],self.bkp[sajatBKP][1],
+					masik.bkp[masikBKP][0],masik.bkp[masikBKP][1]
+				],
+			]
+
 		for egyVonal in vonalak:
-			self.canvas.create_line(egyVonal, width = self.meret * 0.03, fill = self.szin)
+			self.canvas.create_line(egyVonal, width = self.meret * 0.03, fill = self.szin)	
 
+	
 
-class Elem(Jel):
+class elem(Jel):
 	def rajz(self):
 		vonalak = [
 			[
@@ -60,7 +94,7 @@ class Elem(Jel):
 		]
 		Jel.rajz(self, vonalak)
 		
-class Kapcsolo(Jel):
+class kapcsolo(Jel):
 	def rajz(self):
 		vonalak = [
 			[
@@ -77,52 +111,54 @@ class Kapcsolo(Jel):
 			],
 		]
 		
-		korok = [
-			[self.x + self.meret * 0.333 - self.r, self.y + self.meret * 0.5 - self.r,
-			self.x + self.meret * 0.333 + self.r, self.y + self.meret * 0.5 + self.r
+		korok= [
+			[
+				self.x+self.meret*0.333-self.r,	self.y+self.meret*0.5-self.r,
+				self.x + self.meret*0.333 + self.r,	self.y+ self.meret*0.5 + self.r
 			],
-			[self.x + self.meret * 0.666 - self.r, self.y + self.meret * 0.5 - self.r,
-			self.x + self.meret * 0.666 + self.r, self.y + self.meret * 0.5 + self.r
+			[
+				self.x+self.meret*0.666-self.r,	self.y+self.meret*0.5-self.r,
+				self.x + self.meret*0.666 + self.r,	self.y+ self.meret*0.5 + self.r
 			],
-		]
+        ]
+		
+		Jel.rajz(self, vonalak,korok)
 
-		Jel.rajz(self, vonalak, korok)
-
-class Lampa(Jel):
+class lampa(Jel):
 	def rajz(self):
 		
-		self.r = self.meret * 0.2
-		dx = self.r / math.sqrt(2)
+		self.r = self.meret*0.2
+		dx = self.r/math.sqrt(2)
+
 		vonalak = [
 			[
-				self.x, 						self.y + self.meret * 0.5,
-				self.x + self.meret * 0.5 - self.r, self.y + self.meret * 0.5,
+				self.x, 	self.y + self.meret * 0.5,
+				self.x + self.meret * 0.5 - self.r,	 	self.y + self.meret * 0.5,
 			],
 			[
-				self.x + self.meret * 0.5 - dx, self.y + self.meret * 0.5 - dx,
-				self.x + self.meret * 0.5 + dx, self.y + self.meret * 0.5 + dx,
+				self.x + self.meret * 0.5 - dx,		self.y + self.meret * 0.5-dx,
+				self.x + self.meret * 0.5 + dx ,	 self.y + self.meret*0.5 + dx,
 			],
 			[
-				self.x + self.meret * 0.5 - dx, self.y + self.meret * 0.5 + dx,
-				self.x + self.meret * 0.5 + dx, self.y + self.meret * 0.5 - dx,
+				self.x + self.meret * 0.5 - dx,		self.y + self.meret * 0.5 + dx,
+				self.x + self.meret * 0.5 + dx ,	 self.y + self.meret*0.5 - dx,
 			],
 			[
-				self.x + self.meret * 0.5 + self.r, self.y + self.meret * 0.5,
-				self.x + self.meret * 1.00, self.y + self.meret * 0.5,
+				self.x + self.meret * 0.5 + self.r,	self.y + self.meret * 0.5,
+				self.x + self.meret * 1.00, self.y + self.meret *0.5,
 			],
+
 		]
 		
-		korok = [
+		korok= [
 			[
-				self.x + self.meret * 0.5 - self.r, 	self.y + self.meret * 0.5 - self.r,
-				self.x + self.meret * 0.5 + self.r, 	self.y + self.meret * 0.5 + self.r
+				self.x+self.meret*0.5-self.r,	self.y+self.meret*0.5-self.r,
+				self.x + self.meret*0.5 + self.r,	self.y+ self.meret*0.5 + self.r
 			],
-		]
+        ]
+		Jel.rajz(self,vonalak,korok)
 
-		Jel.rajz(self, vonalak, korok)
-
-
-class Ellenallas(Jel):
+class Ellenállás(Jel):
 	def rajz(self):
 
 		vonalak = [
@@ -147,27 +183,24 @@ class Ellenallas(Jel):
 		
 		Jel.rajz(self,vonalak)
 
-from tkinter import *
 import math
+from tkinter import *
 
-win=Tk()
+win = Tk()
 win.geometry("600x620+100+20")
-win.title("Áramkör 10.B/1 2024")
-canvas = Canvas(win, bg = "white")
+win.title("Áramkör")
+canvas = Canvas(win, bg="white")
+print(canvas)
 
-canvas.pack(fill = BOTH, expand = 1)
 
+canvas.pack(fill=BOTH, expand= 1)
 
-elem1 = Elem(0, 0, 100, canvas)
+#elem1 = elem(0,0,100,canvas)
 #elem1.rajz()
 
-elem2 = Elem(200, 100, 50, canvas)
-#elem2.rajz()
-#elem2.szin = "red"
-#elem2.rajz()
-kapcsolo1 = Kapcsolo(350, 150, 100, canvas)
-lampa1 = Lampa(0, 150, 100, canvas)
-ellenallas1 = Ellenallas(500,150,100, canvas)
-
-kapcsolo1.vezetek(ellenallas1)
+kapcsolo1 = kapcsolo(200, 200, 100, canvas)
+lampa1 = lampa(99, 10, 100, canvas)
+#ellenallas1 = Ellenállás(500,150,100, canvas)
+lampa1.vezetek(kapcsolo1,sajatBKP=1,masikBKP =0)
+lampa1.vezetek(kapcsolo1,sajatBKP=1,masikBKP =1)
 win.mainloop()
