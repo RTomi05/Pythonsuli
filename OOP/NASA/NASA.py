a2d0e062a49a10a5952656f6ba1fa67860e6712f
RTomi05 <rengel.tamas.22@ady-nagyatad.hu>
2024-03-20 08:11:48 +0100

    nasa

--- OOP/NASA/NASA.py
+++ OOP/NASA/NASA.py
@@ -1,58 +1,41 @@
-<<<<<<< HEAD
-from NASAclass import *
-=======
 from NASAClass import *
->>>>>>> 4ca3186d9881e346429529c94788fd8db259a1a0
 
 adatok = []
 f = open("NASAlog.txt")
 for egySor in f:
-<<<<<<< HEAD
-    adatok.append(nasa(egySor.strip("\n")))
-=======
-    adatok.append(Nasa(egySor))
->>>>>>> 4ca3186d9881e346429529c94788fd8db259a1a0
+    adatok.append(Nasa(egySor.strip()))
 
-#print(len(adatok))
 f.close()
 
-<<<<<<< HEAD
 print("5. feladat: Kérések száma: {}".format(len(adatok)))
-=======
-print("5. feladat: Kérések száma: {}". format(len(adatok)))
->>>>>>> 4ca3186d9881e346429529c94788fd8db259a1a0
 
 osszeg = 0
 for egyAdat in adatok:
     osszeg += egyAdat.ByteMeret()
+#print(osszeg)
 
-<<<<<<< HEAD
-print("6. feladat: Válaszok összes mérete: {} byte".format(osszeg))
+print("6.feladat: Válaszok összes mérete: {} byte".format(osszeg))
 
-darab = 0
-for egyAdat in adatok:
-    if egyAdat.Domain:
+darab = 0 
+for egyDomain in adatok:
+    if egyDomain.Domain:
         darab += 1
 
-print("8. feladat: Domain-es kérések: {:.2%}".format(darab / len(adatok)))
+print("8.feladat: Domain-es kérések: {:.2%}".format(darab/len(adatok)))
 
 stat = {}
 for egyAdat in adatok:
     if egyAdat.status in stat.keys():
-        stat[egyAdat.status] += 1
+        stat[egyAdat.status]+=1
     else:
-        stat[egyAdat.status] = 1
-
+        stat[egyAdat.status] =1
+'''  
     #másik
-    #try:
-    #    stat[egyAdat.status] += 1
-    #except:
-    #    stat[egyAdat.status] = 1
-
-print("9. feladat: Statisztika:")
+    try:
+        stat[egyAdat.status]+=1
+    except:
+        stat[egyAdat.status]=1
+''' 
+print("9.feladat: Statisztika:")
 for elem in stat:
-    print("\t{}: {} db".format(elem, stat[elem]))
-=======
-print(osszeg)
-
->>>>>>> 4ca3186d9881e346429529c94788fd8db259a1a0
+    print("\t{}:{} db".format(elem,stat[elem]))
