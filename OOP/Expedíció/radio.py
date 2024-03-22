fea8c50c27c76cce1f092c2ca64c7d12669aac3b
RTomi05 <rengel.tamas.22@ady-nagyatad.hu>
2024-03-22 10:35:33 +0100

    exp

---
+++ OOP/Expedíció/radio.py
@@ -0,0 +1,21 @@
+import radioClass as rc
+
+f = open("veetel.txt")
+sorszam = 0
+tempSor = ""
+uzenetek = []
+
+for egySor in f:
+    if sorszam %2 == 0:
+        tempSor = egySor
+    else:
+        uzenetek.append(rc.Uzenet(tempSor, egySor))
+
+    sorszam += 1
+    #radiátor továbbképzés
+      
+f.close()
+
+print("2. feladat")
+print("Az első üzenet rögzítője: {}".format(uzenetek[0].amator))
+print("Az utolsó üzenet rögzítője: {}".format(uzenetek[-1].amator))
--- OOP/Expedíció/radioClass.py
+++ OOP/Expedíció/radioClass.py
@@ -1,5 +1,16 @@
-class uzenet:
+class Uzenet:
     def __init__(self,sor1,sor2):
         #"2 15"
         #"1/0 #gy#domb##l fig###tu# f#i#s ho#a##dalyoz$$"
         self.nap = int(sor1.split(" ")[0])
+        self.amator = int(sor1.split(" ")[1])
+
+        self.uzenet = sor2
+
+    def farkasKereso(self):
+        return "farkas" in self.uzenet
+    
+        if "farkas" in self.uzenet:
+            return True
+        else:
+            return False
