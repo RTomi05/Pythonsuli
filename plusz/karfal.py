6f5cfc3e2139ea523a0bae19af7b8f85ca462210
RTomi05 <rengel.tamas.22@ady-nagyatad.hu>
2024-04-25 16:51:39 +0200

    majdnemjo

--- plusz/karfal.py
+++ plusz/karfal.py
@@ -1,8 +1,30 @@
 
-#Ő itt Ted,a  kis karakterünk
+#Ő itt Ted, a kis karakterünk
+
+magan = ["a", "á", "e", "é", "i", "í", "o", "ó", "ö", "ő", "u", "ú", "ü", "ű"]
+rossz =  []
 
 def Ted():
+
+    global rossz
     while True:
-        beadott = input("Adj meg EGY karaktert! ")
-        if len(beadott) == 1:
-            print("Szia")
+        beadott = input("Adj meg EGY betűt! ")
+        if len(beadott) == 1 and beadott != " ":
+            if beadott in magan:
+                print("Ez finom volt!")
+            else:
+                print("Fúj!")
+                rossz.append(beadott)
+                if len(rossz) == 10:
+                    print("Ezeket vidd innen: ", rossz)
+                    rossz = []
+                else:
+                    pass
+
+        elif len(beadott) != 1 or beadott == " ":
+            print("Ez nem egy betű volt!")
+            #Ted()
+
+#Program:
+
+Ted()
