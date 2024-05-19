243d6cdc7f8160def58726a721a033d5002f4229
RTomi05 <rengel.tamas.22@ady-nagyatad.hu>
2024-05-19 18:08:07 +0200

    szamosit.py

---
+++ plusz/szamosit.py
@@ -0,0 +1,17 @@
+import random
+
+#A gép száma:
+gep = random.randint(0,2)
+rossz = 0
+
+while rossz <= 2:
+    player = int(input("Szerinted mire gondolt a gép? "))
+    if player == gep:
+        print("Eltaláltad!")
+        break
+    else:
+        print("Sajnos nem jött össze!")
+        rossz += 1
+
+if rossz == 3:
+    print("Sajnos elfogytak a lehetőségeid.")
