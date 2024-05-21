a848e8c2ef1543c2061d0d9c7372ae28926e7a7d
RTomi05 <rengel.tamas.22@ady-nagyatad.hu>
2024-05-21 12:35:56 +0200

    memoriajatek

---
+++ Gyakorlás/szolanc.py
@@ -0,0 +1,17 @@
+
+megy = 0
+hanyadik = 1
+szavak = []
+
+while megy == 0:
+    if hanyadik == 1:
+        szo = input("Kérem a(z) {} szót! ".format(hanyadik))
+        elozoSzo = szo
+        szavak.append(szo)
+        hanyadik += 1
+    elif hanyadik > 1:
+        print("Hali")
+        szo = input("Kérem a(z) {} szót! ".format(hanyadik))
+        if elozoSzo[-1] == szo[1]:
+            hanyadik += 1
+    
